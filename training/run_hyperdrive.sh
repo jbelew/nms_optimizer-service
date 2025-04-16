e5eb20a562db8059b767bfe3dc0c71a17ba4ec9a
John Belew <john.belew@gmail.com>
2025-04-16 10:20:57 -0700

    Fixed the hyperdrive generation script.

--- training/run_hyperdrive.sh
+++ training/run_hyperdrive.sh
@@ -4,9 +4,7 @@ rsync -vrt --progress generated_batches/ /mnt/media/projects/nms_optimizer/train
 rm -rf generated_batches/
 
 while true; do
-    python generate_data.py --ship living --category Weaponry
-    python generate_data.py --ship living --category Hyperdrive
-    python generate_data.py --ship living --category "Defensive Systems"
+    python generate_data.py --ship standard --category Hyperdrive
     rsync -vrt --progress generated_batches/ /mnt/media/projects/nms_optimizer/training/
     echo "Program exited. Restarting..."
     sleep 1
