4e997af73cf00eb58e5ca6f0b58a6dff6ddc1e59
John Belew <john.belew@gmail.com>
2025-04-17 18:14:57 -0700

    New models and data.

--- training/run_multitools.sh
+++ training/run_multitools.sh
@@ -9,7 +9,7 @@ while true; do
     python generate_data.py --ship standard-mt --category Scanners
     python generate_data.py --ship standard-mt --category Weaponry
     python generate_data.py --ship atlantid --category Mining
-    # rsync -vrt --progress generated_batches/ /mnt/media/projects/nms_optimizer/training/
+    rsync -vrt --progress generated_batches/ /mnt/media/projects/nms_optimizer/training/
     echo "Program exited. Restarting..."
     sleep 1
 done
