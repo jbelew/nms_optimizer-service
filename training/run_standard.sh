3f4a25dbfefe7ef9e9f8f90d65948d577e7bf7d0
John Belew <john.belew@gmail.com>
2025-04-23 11:16:47 -0700

    Data updates.

--- training/run_standard.sh
+++ training/run_standard.sh
@@ -3,16 +3,16 @@
 rm -rf generated_batches/
 
 while true; do
-    python generate_data.py --ship standard --category Weaponry
-    # python generate_data.py --ship standard --category Hyperdrive
-    # python generate_data.py --ship standard --category "Defensive Systems"
-    # python generate_data.py --ship living --category Weaponry
-    # python generate_data.py --ship living --category Hyperdrive
-    # python generate_data.py --ship living --category "Defensive Systems"
-    # python generate_data.py --ship standard-mt --category "Weaponry"
-    # python generate_data.py --ship standard-mt --category "Scanners"
-    # python generate_data.py --ship standard-mt --category "Mining"
-    # rsync -vrt --progress generated_batches/ /mnt/media/projects/nms_optimizer/training/
+    #python generate_data.py --ship standard --category Weaponry
+    python generate_data.py --ship standard --category Hyperdrive
+    python generate_data.py --ship standard --category "Defensive Systems"
+    python generate_data.py --ship living --category Weaponry
+    python generate_data.py --ship living --category Hyperdrive
+    python generate_data.py --ship living --category "Defensive Systems"
+    python generate_data.py --ship standard-mt --category "Weaponry"
+    python generate_data.py --ship standard-mt --category "Scanners"
+    python generate_data.py --ship standard-mt --category "Mining"
+    rsync -vrt --progress generated_batches/ /mnt/media/projects/nms_optimizer/training/
     echo "Program exited. Restarting..."
     sleep 1
 done
