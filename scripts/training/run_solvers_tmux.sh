d43d56ba6afa5d2fe4b2b5a2fdf963157f0db4de
John Belew <john.belew@gmail.com>
2025-09-14 10:39:21 -0700

    Adding more data for the ML models

--- scripts/training/run_solvers_tmux.sh
+++ scripts/training/run_solvers_tmux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SESSION_NAME="nms_solvers"
-BASE_CMD="cd /home/jbelew/projects/nms_optimizer-service && ./venv/bin/python -m scripts.training.generate_data --ship corvette --category \"Hyperdrive\" --tech hyper --solve_type max"
+BASE_CMD="cd /home/jbelew/projects/nms_optimizer-service && ./venv/bin/python -m scripts.training.generate_data --ship corvette --category \"Defensive Systems\" --tech shield --solve_type max"
 
 /usr/bin/tmux kill-session -t $SESSION_NAME 2>/dev/null
 /usr/bin/tmux new-session -d -s $SESSION_NAME
