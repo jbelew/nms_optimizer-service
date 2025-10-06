62ea6aba400aefa1c3b4716e195edd721a32df86
John Belew <john.belew@gmail.com>
2025-10-05 23:05:39 -0700

    Removed dead file

--- src/optimization_context.py
+++
@@ -1,7 +0,0 @@
-"""
-This module is intended to hold context-related information for the optimization process.
-
-It can be used to store and pass around state between different parts of the
-optimization pipeline, such as the main optimization function, the ML placement
-model, and the refinement algorithms.
-"""
